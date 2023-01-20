3361ae1f332ca8651103d5822ab05815b5e32161
mikhail.madoyan <mikhail.madoyan@gmail.com>
2023-01-20 17:33:21 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson_21_SelectionSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 56, 1, 99, 67, 89, 6, 44, 12, 78, 34 };
+
+            int n = 10;
+            
+            int temp, small;
+
+            for (int i = 0; i < n - 1; i++)
+            {
+                small = i;
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[j] < arr[small])
+                    {
+                        small = j;
+                    }
+                }
+                temp = arr[small];
+                arr[small] = arr[i];
+                arr[i] = temp;
+            }
+            
+            Console.Write("Sorted: ");
+            for (int i = 0; i < n; i++)
+            {
+                Console.Write(arr[i] + " ");
+            }
+            Console.ReadLine();
+        }
+    }
+}
